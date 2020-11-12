d1a929e5c4019d723e618abd10669d589dbcd574
inacioreina <74356253+inacioreina@users.noreply.github.com>
2020-11-12 12:14:20 +0000

    NavMesh

---
+++ Assets/Scripts/Inácio/Traffic/AI_Car/CarAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public Vector3 targetDestination;
+
+    private WaypointManager waypoint;
+
+    private void Awake()
+    {
+        waypoint = FindObjectOfType<WaypointManager>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        targetDestination = waypoint.waypoints[Random.Range(0, waypoint.waypoints.Count - 1)];
+        agent.destination = targetDestination;
+    }
+
+    private void Rotate()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        agent.transform.Rotate(0, 0, 10);
+    }
+}
---
+++ Assets/Scripts/Inácio/WaypointManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    public List<Vector3> waypoints = new List<Vector3>();
+
+    private void Awake()
+    {
+        foreach (Transform waypoint in transform)
+        {
+            waypoints.Add(waypoint.position);
+        }
+    }
+}
