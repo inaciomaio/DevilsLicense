340864aa73f9453a7673cfd3e6ff8e303487c552
inacioreina <74356253+inacioreina@users.noreply.github.com>
2020-11-23 13:48:57 +0000

    Update 0.15
    -New Waypoint system
    -ProGrids for easy level construction
    -Road Prefab with repeating textures
    -Car Movement still needs working

---
+++ Assets/Scripts/Inácio/Traffic/WaypointNavigator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNavigator : MonoBehaviour
+{
+    private CarAI car;
+    public Waypoint currentWaypoint;
+
+    private void Awake()
+    {
+        car = GetComponent<CarAI>();
+    }
+
+    private void Start()
+    {
+        car.SetDestination(currentWaypoint.GetPosition());
+    }
+
+    private void Update()
+    {
+        if (car.HasReachedDestination)
+        {
+            currentWaypoint = currentWaypoint.NextWaypoint;
+            car.SetDestination(currentWaypoint.GetPosition());
+        }
+    }
+}
