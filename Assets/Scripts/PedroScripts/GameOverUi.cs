96d1020c37962dca8fc2571a0bf078baa2005a48
barbosayyy <pedro_barbosa2000@hotmail.com>
2020-12-10 02:58:14 +0000

    0.15.9
    -Added Game Over

---
+++ Assets/Scripts/PedroScripts/GameOverUi.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverUi : MonoBehaviour
+{
+    private GameObject _youLost;
+    private GameObject _youWon;
+    
+    private TextMeshProUGUI _errorText;
+    
+    private Manager _manager;
+    void Awake()
+    {
+        _youLost = GameObject.Find("YouLostText");
+        _youWon = GameObject.Find("YouWonText");
+        _errorText = GameObject.Find("ErrorsText").GetComponent<TextMeshProUGUI>();
+    }
+    
+    void Start()
+    {
+        _manager = GameObject.Find("Manager").GetComponent<Manager>();
+        
+        _youLost.SetActive(false);
+        _youWon.SetActive(false);
+        
+        if (_manager.errorCount >= 3)
+        {
+            _youWon.SetActive(true);
+        }
+        else
+        {
+            _youLost.SetActive(true);
+        }
+
+        _errorText.text = "Errors:" + " " + _manager.errorCount;
+    }
+    
+    public void RetryButton()
+    {
+        _manager.errorCount = 0;
+        SceneManager.LoadScene(_manager.currentSceneIndex);
+    }
+
+    public void ToMenuButton()
+    {
+        _manager.errorCount = 0;
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/PedroScripts/LevelEnd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelEnd : MonoBehaviour
+{
+    private Manager _manager;
+    private UnityEvent _gameOver;
+    void Start()
+    {
+        _manager = GameObject.Find("Manager").GetComponent<Manager>();
+        _gameOver = new UnityEvent();
+        _gameOver.AddListener(_manager.OnGameOverMthd);
+    }
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Car"))
+        {
+            _gameOver.Invoke();
+        }
+    }    
+}
+
+
