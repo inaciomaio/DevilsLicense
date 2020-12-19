143c9d483c4dc6d87f6972d8226724ab0aec9bdc
barbosayyy <pedro_barbosa2000@hotmail.com>
2020-12-19 04:33:51 +0000

    1.15.10
    -Added Spawnable Crosswalk (WIP)

---
+++ Assets/Scripts/PedroScripts/CrosswalkSign.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosswalkSign : MonoBehaviour
+{
+    public GameObject spawnCw;
+    public GameObject cwPrefab; //For test, it will be a sprite !! REPLACE FOR PREFAB OBJECT !!
+
+    public bool canSpawn = true;
+    void Start()
+    {
+        cwPrefab = Resources.Load<GameObject>("Prefabs/passadeira");
+    }
+
+    public void OnPointerEnter()
+    {
+        Debug.Log("TestPointer");
+    }
+
+    public void ClickSpawn()
+    {
+        if (canSpawn)
+        {
+            canSpawn = false;
+            Instantiate(cwPrefab, spawnCw.transform);
+        }
+    }
+}
