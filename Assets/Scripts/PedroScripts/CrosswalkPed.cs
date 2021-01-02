bff45f7d54aff3d3a942011559dce307859bc335
barbosayyy <pedro_barbosa2000@hotmail.com>
2021-01-02 23:35:59 +0000

    1.15.11
    -Finished Crosswalk Prefab

---
+++ Assets/Scripts/PedroScripts/CrosswalkPed.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosswalkPed : MonoBehaviour
+{
+    public float Speed;
+    public float Wait = 1f;
+    void Awake()
+    {
+        StartCoroutine(WaitThis());
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * Speed);
+    }
+
+    IEnumerator WaitThis()
+    {
+        yield return new WaitForSeconds(Wait);
+        DestroyObj();
+    }
+
+    void DestroyObj()
+    {
+        Destroy(gameObject);
+    }
+}
