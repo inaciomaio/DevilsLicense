1612625bdb7dec861622cf43ab9978d51fa25bae
inacioreina <74356253+inacioreina@users.noreply.github.com>
2021-01-06 15:26:40 +0000

    1.16.1.1
    -Fixed a minor issue with the NavMesh baking colliders.

---
+++ Assets/Destination.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destination : MonoBehaviour
+{
+    public Vector3 Position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Inácio/DisableRoadBakingColliders.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableRoadBakingColliders : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            Destroy(child.GetComponent<PolygonCollider2D>());
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
