f93b700879e2fc9282c0cae98d7dad4694187c2c
inacioreina <74356253+inacioreina@users.noreply.github.com>
2021-01-07 10:40:15 +0000

    test

--- Assets/Scripts/Inácio/Traffic/AI/PedAI.cs
+++ Assets/Scripts/Inácio/Traffic/AI/PedAI.cs
@@ -1,22 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 public class PedAI : MonoBehaviour
 {
     private PedDestinationManager manager;
-    public NavMeshAgent Agent;
+    private NavMeshAgent agent;
+    public int DestinationIndex;
+    public Transform CurrentDestinationTransform;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
+        manager = FindObjectOfType<PedDestinationManager>();
+        agent = transform.GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = Random.Range(.3f, 1f);
 
+
+        // Update is called once per frame
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-       
+        
+        if(CurrentDestinationTransform == null)
+        {
+            var index = GenerateNewIndex();
+            CurrentDestinationTransform = manager.Destinations[index];
+            agent.SetDestination(CurrentDestinationTransform.position);
+            
+        }
+
+        Debug.Log("Destination 0: " + manager.Destinations[0]);
+        Debug.Log("Destinations Count" + manager.Destinations.Count);
+
+        if(Vector3.Distance(transform.position, manager.Destinations[DestinationIndex].position) <= agent.stoppingDistance)
+        {
+            var index = GenerateNewIndex();
+            agent.SetDestination(manager.Destinations[index].position);
+            agent.stoppingDistance = Random.Range(.3f, 1f);
+
+        }
+        
+    }
+
+    private int GenerateNewIndex()
+    {
+        if (manager.Destinations.Count != 0)
+        {
+            DestinationIndex = Random.Range(0, manager.Destinations.Count - 1);
+            Debug.Log("random number - " + DestinationIndex);
+        }
+
+        return DestinationIndex;
     }
 }
