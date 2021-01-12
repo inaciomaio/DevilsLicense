fcc0860e6b01d18dceb1accecbdcb154a14b37b9
barbosayyy <pedro_barbosa2000@hotmail.com>
2021-01-12 10:25:11 +0000

    1.17
    -Soundddd

---
+++ Assets/Scripts/PedroScripts/AudioOut.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOut : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip hoverbutton;
+    public float volume = 0.5f;
+    public bool hasPlayed = false;
+    void Awake()
+    {
+        audioSource.clip = hoverbutton;
+    }
+
+    void OnMouseOver()
+    {
+        if (hasPlayed == false)
+        {
+            audioSource.Play();
+            hasPlayed = true;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        hasPlayed = false;
+        audioSource.PlayOneShot(hoverbutton, volume);
+    }
+}
