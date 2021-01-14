2c90dcfd99e2f8f872e6cdfab43b26b1742699cb
barbosayyy <pedro_barbosa2000@hotmail.com>
2021-01-14 17:07:55 +0000

    1.17.3
    -Menu button animations
    -Menu Options WIP

---
+++ Assets/Scripts/GlobalManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalManager : MonoBehaviour
+{
+    public float Volume;
+    public float changeableVolume;
+    public Slider volumeSlider;
+    void Start()
+    {
+        Volume = AudioListener.volume;
+    }
+
+    void Update()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+}
