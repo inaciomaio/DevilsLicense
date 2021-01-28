7afcf75cd54ad7b5f6a8407236a985a0a9ef7749
inacioreina <74356253+inacioreina@users.noreply.github.com>
2021-01-28 01:57:59 +0000

    1.21.1
    -Implemented car sound

--- Assets/Scripts/Inácio/Traffic/CarSound.cs
+++ Assets/Scripts/Inácio/Traffic/CarSound.cs
@@ -3,49 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CarAI))]
+[RequireComponent(typeof(AudioClip))]
 public class CarSound : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
-
-    public enum EngineAudioOptions
-    {
-        Simple,
-        FourChannel
-    }
-
-    public EngineAudioOptions EngineSoundStyle = EngineAudioOptions.FourChannel;
-    public AudioClip LowAccelClip;
-    public AudioClip LowDecelClip;
-    public AudioClip HighAccelClip;
-    public AudioClip HighDecelClip;
-    public float PitchMultiplier = 1f;
-    public float LowPitchMin = 1f;
-    public float LowPitchMax = 6f;
-    public float HighPitchMultiplier = .25f;
-    public float MaxRollOffDistance = 500;
+    private CarAI car;
+    public AudioClip IdleClip;
+    public float PitchMin = 1.45f;
+    public float PitchMax = 1.85f;
     public float DopplerLevel = 1;
     public bool UseDoppler = true;
 
-    private AudioSource lowAccel;
-    private AudioSource lowDecel;
-    private AudioSource highAccel;
-    private AudioSource highDecel;
+    private AudioSource source;
     private bool startedSound;
-    private CarAI CarAI;
+
+
+    void Start()
+    {
+        car = GetComponent<CarAI>();
+        StartSound();
+    }
+
+
 
     private void StartSound()
     {
-        //highAccel = SetUpEngineAudioSource(HighAccelClip);
+        startedSound = true;
+        SetupAudioSource(IdleClip);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //float camDist = (Camera.main.transform.position - transform.position).sqrMagnitude;
+        
+        if(startedSound)
+        {
+            if(!car.CanDrive)
+            {
+                source.pitch = 1;
+            }
+            else if(car.TargetSpeed > 5)
+            {
+                source.pitch = PitchMax;
+            }
+            else if(car.TargetSpeed == 5f)
+            {
+                source.pitch = PitchMin;
+            }
+        
+
+        }
+
+    }
+
+    private AudioSource SetupAudioSource(AudioClip clip)
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.volume = .25f;
+        source.loop = true;
+
+        source.time = Random.Range(0f, clip.length);
+        source.Play();
+        source.minDistance = 5;
+        source.dopplerLevel = 0;
+        return source;
     }
 
 }
